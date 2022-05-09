55585b00d345515ddbb6df6f0945822f7a44c783
Aaron Boyd <a.boyd91@yahoo.com>
2022-05-08 23:36:23 -0700

    add routing requests for words functionality

---
+++ app.rb
@@ -0,0 +1,51 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/word')
+require('pry')
+also_reload('lib/**/*.rb')
+
+get('/') do
+  @words = Word.all
+  erb(:words)
+end
+
+get('/words') do
+  @words = Word.all
+  erb(:words)
+end
+
+get('/words/new') do
+  erb(:new_word)
+end
+
+post('/words') do
+  word = params[:word_word]
+  word = Word.new(word, nil)
+  word.save()
+  @words = Word.all()
+  erb(:words)
+end
+
+get('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  erb(:word)
+end
+
+get('/words/:id/edit') do
+  @word = Word.find(params[:id].to_i())
+  erb(:edit_word)
+end
+
+patch('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  @word.update(params[:word])
+  @words = Word.all
+  erb(:words)
+end
+
+delete('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  @word.delete()
+  @words = Word.all
+  erb(:words)
+end
