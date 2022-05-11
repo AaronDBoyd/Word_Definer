c248032f0272059b09cb227967b0b2cc98fee691
Aaron Boyd <a.boyd91@yahoo.com>
2022-05-10 23:44:03 -0700

    add all passing tests for Word and Definition CRUD functionality

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -3,12 +3,117 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe('create word path', {:type => :feature}) do
-  it('creates a word and then goes to the word page') do
+describe('#Word CRUD functionality', {:type => :feature}) do
+  it('CREATES a word and then goes to the words page') do
     visit('/words')
     click_on('Add a new word')
     fill_in('word_name', :with => 'House')
     click_on('Go!')
     expect(page).to have_content('House')
   end
+
+  it('READS a word and directs to its page by clicking on it') do 
+    house = Word.new("House123", nil)
+    house.save()
+    visit('/words')
+    click_on("House123")
+    expect(page).to have_content('House123')
+  end
+
+  it('UPDATES a word') do 
+    pencil = Word.new("Pencil", nil)
+    pencil.save()
+    visit("/words/#{pencil.id}/edit")
+    fill_in('name', :with => "Marker")
+    click_on('Update')
+    expect(page).to have_content("Marker")
+  end
+
+  it('DELETES a word') do
+    Word.clear
+    pencil = Word.new("Pencil", nil)
+    pencil.save()
+    marker = Word.new("Marker", nil)
+    marker.save()
+    visit("/words/#{marker.id}/edit")
+    click_on("Delete")
+    expect(page).to have_no_content('Marker')
+  end
 end
+
+describe('#Definition CRUD functionality', {:type => :feature}) do 
+  it('CREATES a definition for a word') do
+    pencil = Word.new("Pencil", nil)
+    pencil.save
+    visit("/words/#{pencil.id}")
+    fill_in('definition_name', :with => 'writing utencil')
+    click_on('Add Definition')
+    expect(page).to have_content('writing utencil')
+  end
+
+  it('READS a definition and directs to its page by clicking on it') do
+    pencil = Word.new("Pencil", nil)
+    pencil.save
+    deff = Definition.new('writing utencil', pencil.id, nil)
+    deff.save
+    visit("/words/#{pencil.id}")
+    click_on('writing utencil')
+    expect(page).to have_content('writing utencil')
+  end
+
+  it('UPDATES a definition') do
+    dragon = Word.new('Dragon', nil)
+    dragon.save
+    deff = Definition.new('big lizard', dragon.id, nil)
+    deff.save
+    visit("/words/#{dragon.id}/definitions/#{deff.id}")
+    fill_in('name', :with => 'mythical creature')
+    click_on('Update definition')
+    expect(page).to have_content('mythical creature')
+
+  end
+
+  it('DELETES a definition') do
+    dragon = Word.new("Dragon", nil)
+    dragon.save
+    deff= Definition.new("big lizard", dragon.id, nil)
+    deff.save()
+    visit("/words/#{dragon.id}/definitions/#{deff.id}")
+    click_on('Delete definition')
+    expect(page).to have_no_content('big lizard')
+  end
+end
+
+# describe('enter a words page', {:type => :feature}) do
+#   it('enters a words page by clicking it and has the word populated') do 
+#     house = Word.new("House123", nil)
+#     house.save()
+#     visit('/words')
+#     click_on("House123")
+#     expect(page).to have_content('House123')
+#   end
+# end
+
+# describe('Update word', {:type => :feature}) do 
+#   it('edits a previously entered word') do 
+#     pencil = Word.new("Pencil", nil)
+#     pencil.save()
+#     visit("/words/#{pencil.id}/edit")
+#     fill_in('name', :with => "Marker")
+#     click_on('Update')
+#     expect(page).to have_content("Marker")
+#   end
+# end
+
+
+
+# describe('add definition', {:type => :feature}) do 
+#   it('adds a definition to a word') do 
+#     pencil = Word.new("Pencil", nil)
+#     pencil.save()
+#     visit("/words/#{word.id}")
+#     fill_in('definition_name', :with => 'object for writing')
+#     click_on('Add Definition')
+#   end
+# end
+
